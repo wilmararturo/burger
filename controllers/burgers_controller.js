2cdead053ddaa31c064d2bcacc2c7e0630706b32
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-11-04 02:14:32 -0500

    updated routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,19 +4,31 @@ const express = require("express");
 const router = express.router();
 
 router.get("/", (req, res) => {
-
+    res.redirect("/burgets");
 });
 
 router.get("/burgers", (req, res) => {
+    burger.all((data) => {
+        const hbsObj = {
+            burgers: data,
+        };
+        res.render("index", hbsObj);
+    })
+        .catch(err => res.json(err.message));
 
 });
 
-router.post("/burgers/create", (req, red) => {
+router.post("/burgers/create", (req, res) => {
+    burger.create(res.body.burger_name, (data) => {
+        res.redirect("/");
+    })
 
 });
 
 router.put("/burgers/:id", (req, res) => {
-
+    burger.update(res.params.id, (data) => {
+        res.sendStatus(200);
+    })
 });
 
 module.exports = router;
