10d5d57603a9c282e2fd17560ce4e926f6af9c84
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-11-04 02:23:27 -0500

    fixed typo

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.redirect("/burgets");
+    res.redirect("/burgers");
 });
 
 router.get("/burgers", (req, res) => {
