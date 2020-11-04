c68c09f15a99d00ec6cdf01b7d5ba77d00922f9d
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-11-04 01:17:26 -0500

    initial commit

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,18 @@
+const burger = require("../models/burger");
+const express = require("express");
+
+const router = express.router();
+
+router.get("/", (req, res) => {
+
+});
+
+router.post("/api/burgers", (req, red) => {
+
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+
+});
+
+module.exports = router;
